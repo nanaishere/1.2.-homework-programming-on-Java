51a19573feebb8608b2f61e5e2cae466404fcc4b
nanaishere <naiavasilyeva@gmail.com>
2021-08-11 11:42:58 +0900

    Added files

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int currentbalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int total = currentbalance + transferAmount;
+        System.out.println(total);
+    }
+}
